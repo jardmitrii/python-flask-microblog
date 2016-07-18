9a1fd73ebe6f6f1e3d48f1456e0e7ae4cbca152f
djar <jar.dmitrii@gmail.com>
2016-07-18 19:04:05 +0300

    Try forms

---
+++ config.py
@@ -0,0 +1,8 @@
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'Jiree7thi6eeT3oobuuKesh3eichahV7oos2ooth3aa3aph7ae'
+
+OPENID_PROVIDERS = [
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
